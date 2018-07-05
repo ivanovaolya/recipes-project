ce9ee49b1180256b73d7199979be06456747a6b4
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-07-05 16:16:36 +0300

    Created Recipe model; edited recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Seafood stew', 'Just a seafood stew',
+      'http://scibosnian.com/wp-content/uploads/2017/12/Seafood_Stew.jpg'),
+    new Recipe('Pancakes', 'Australias pancakes',
+      'https://img.taste.com.au/GR-XKpyy/taste/2014/10/australias-most-cooked-pancake-recipe-118377-2.jpg')
+  ];
 
   constructor() { }
 
