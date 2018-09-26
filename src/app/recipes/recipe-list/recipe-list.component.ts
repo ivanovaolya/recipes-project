918a7c512000dafbb616b22f5e52d6818bcdf92f
Olha Melnyk <Olha_Melnyk@epam.com>
2018-09-26 13:21:46 +0300

    Refactored using a service for cross-component communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -11,16 +11,10 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>(); // differ from the name in RecipeItemComponent
-
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(selectedRecipe: Recipe) {
-    this.recipeWasSelected.emit(selectedRecipe);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Seafood stew', 'Just a seafood stew',
       'https://f.wishabi.net/flyers/520b2d19-993a-4975-86d0-efb8f600c019/2_1_1.jpg'),
