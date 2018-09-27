ce22b7cd43175084b305e5a208568d2e922088ea
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-09-27 11:19:41 +0300

    Added ingredients array to Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
@@ -7,9 +8,11 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('Seafood stew', 'Just a seafood stew',
-      'https://f.wishabi.net/flyers/520b2d19-993a-4975-86d0-efb8f600c019/2_1_1.jpg'),
+      'https://f.wishabi.net/flyers/520b2d19-993a-4975-86d0-efb8f600c019/2_1_1.jpg',
+      [new Ingredient('Fish', 1), new Ingredient('Rice', 23)]),
     new Recipe('Pancakes', 'Australias pancakes',
-      'https://static.wixstatic.com/media/e5e583_fb9627a243f7493095863e0ff21f4eaf~mv2_d_2448_2448_s_4_2.jpg_256')
+      'https://static.wixstatic.com/media/e5e583_fb9627a243f7493095863e0ff21f4eaf~mv2_d_2448_2448_s_4_2.jpg_256',
+      [new Ingredient('Flour', 50), new Ingredient('Water', 100), new Ingredient('Sugar', 24)])
   ];
 
   getRecipes() {
