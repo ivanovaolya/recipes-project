b7a5ffc554236b8d463834f3a9424447204a89fe
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-10-18 13:01:52 +0300

    Added signing users up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,15 @@
-export class AuthService {
-  signupUser(email: string, password: string) {
+import * as firebase from 'firebase';
 
+export class AuthService {
+
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(
+        (response) => console.log(response)
+      )
+      .catch(
+        (error) => console.log(error)
+      );
   }
+
 }
