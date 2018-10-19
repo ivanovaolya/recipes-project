40ceddcd2663b13b55d1e718675243007d592b29
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-10-19 16:09:31 +0300

    Added check for authenticated status; changed header behaviour

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 })
 export class HeaderComponent {
 
-  constructor(private dataStorage: DataStorageService) {}
+  constructor(private dataStorage: DataStorageService, private authService: AuthService) {}
 
   onSaveData() {
     this.dataStorage.storeRecipes()
