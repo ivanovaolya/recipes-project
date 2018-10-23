7d68ae6fe4b3388b90f97c72fc21cd05f80381db
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-10-23 13:03:54 +0300

    Added HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },  // redirect only if the full path is empty
+  { path: '', component: HomeComponent },  // redirect only if the full path is empty
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
