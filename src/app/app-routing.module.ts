883183a7a1380d624bf808719d0a7b7ed4ad30bc
ivanovaolya <olya.ivanova.kiev@gmail.com>
2018-10-23 13:10:59 +0300

    Added lazy loading to the RecipesModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },  // redirect only if the full path is empty
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }, // dynamically load RecipesModule
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
